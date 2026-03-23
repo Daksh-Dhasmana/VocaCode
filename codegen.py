a79a03951e77252ec1de45bb503e39bab9ba00d6
MANAV BEDI <manavbedi.work@gmail.com>
2026-03-24 01:44:47 +0530

    manavp2

--- codegen.py
+++ codegen.py
@@ -1,14 +1,32 @@
 def generate_c(ir):
     lines = ir.strip().split('\n')
     c_lines = []
+    declared = set()
+
+    c_lines.append("#include <stdio.h>")
+    c_lines.append("int main() {")
+
     for line in lines:
         line = line.strip()
         if not line:
             continue
+
         if line.startswith("print "):
-            var = line.split(" ")[1]
-            c_lines.append(f'printf("%d\\n", {var});')
+            var = line.split()[1]
+            c_lines.append(f'    printf("%d\\n", {var});')
+
         elif "=" in line:
             var, val = line.split('=', 1)
-            c_lines.append(f"int {var.strip()} = {val.strip()};")
+            var = var.strip()
+            val = val.strip()
+
+            if var not in declared:
+                c_lines.append(f"    int {var} = {val};")
+                declared.add(var)
+            else:
+                c_lines.append(f"    {var} = {val};")
+
+    c_lines.append("    return 0;")
+    c_lines.append("}")
+
     return "\n".join(c_lines)
